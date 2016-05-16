45f0abf6a7e3e4580c2f7e878d8dea4383a96f69
Robin DIDIER <rdidier@e3r7p23.42.fr>
2016-05-16 11:44:26 +0200

    On commence notre premier vrai site web

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Camagru by Mathiisss</title>
+    <link rel="stylesheet" href="index.css">
+</head>
+<body>
+<div class="header box_design"> header </div>
+    <div class="main box_design"> main ici</div>
+    <div class="side box_design"> Side ici</div>
+<div class="footer box_design"> Footeur ici </div>
+</body>
+</html>
